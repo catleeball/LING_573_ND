759da5cc9d6a20cc876d058aced495fc6a7a0e35
Emma McKibbin <echm@patas.ling.washington.edu>
2024-05-04 13:33:21 -0700

    cleaned up D3_run file

--- src/D3_run_evaluate.sh
+++ src/D3_run_evaluate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 PROJECT_ROOT="$(pwd)"   # change if necessary, this works from D3.cmd
-# toy dataset
-# EVAL_DATA_FILE="$PROJECT_ROOT/data/sarc/toy_comments-train-balanced.json"
-# dev dataset
-EVAL_DATA_FILE="$PROJECT_ROOT/data/sarc/dev-comments-balanced.json"
 
+# NOTE: if you do not have dev-comment-balanced.json, comment in partition lines (21-23) instead
+EVAL_DATA_FILE="$PROJECT_ROOT/data/sarc/dev-comments-balanced.json"
 
 # models to evaluate:
 BASE_MODEL_NAME="dabagyan/bert-sarcasm-model"
@@ -19,7 +17,7 @@ COMBINED_MODEL_OUT_FILE="$PROJECT_ROOT/outputs/D3/d3_combined.out"
 
 METRICS_FILE="$PROJECT_ROOT/results/D3_scores.out"
 
-# preprocess data - COMMENT OUT 3 LINES BELOW IF ALREADY DONE (in D2)
+# partition data - COMMENT OUT 3 LINES BELOW IF ALREADY DONE (in D2)
 # cd $PROJECT_ROOT/src/dev_partition/
 # ./make_dev_set.sh $EVAL_DATA_FILE
 # cd $PROJECT_ROOT
