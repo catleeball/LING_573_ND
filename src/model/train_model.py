fa6018a944fcb90d83cfba0b34d4848ae999ad44
Diana Abagyan <dabagyan@uw.edu>
2024-05-03 21:00:36 -0400

    roberta

--- src/model/train_model.py
+++ src/model/train_model.py
@@ -2,6 +2,7 @@ import json
 import argparse
 import torch
 from transformers import AutoTokenizer, BertForSequenceClassification
+from transformers import RobertaTokenizerFast, RobertaForSequenceClassification
 from transformers import TrainingArguments, Trainer
 from pathlib import Path
 from .utils import *
@@ -9,10 +10,9 @@ from .utils import *
 
 parser = argparse.ArgumentParser(description='Finetune BERT model for sarcasm detection.')
 parser.add_argument('--context', action='store_true', help='Whether or not to use post context.')
+parser.add_argument('--roberta', action='store_true', help='Whether to use RoBERTa or BERT(default).')
 args = parser.parse_args()
 
-
-# project_root="~/LING_573_ND/"
 project_root = Path(__file__).cwd()
 data_dir = project_root / "data" / "sarc"
 model_dir = project_root / "outputs" / "context-models" if args.context else project_root / "outputs" / "base-models"
@@ -22,15 +22,20 @@ eval_filename = data_dir / "dev-comments-balanced.json"
 
 # LOAD MODEL
 print("Loading model...")
-pretrained_checkpoint = "google-bert/bert-base-uncased"  
+pretrained_checkpoint = "google-bert/bert-base-uncased" if args.roberta else "google-bert/bert-base-uncased" 
 id2label = {0: "not_sarcastic", 1: "sarcastic"} 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Training model using device: {device}")
 print(f"Training context model: {args.context}")
+print(f"Using pretrained: {pretrained_checkpoint}")
 
-tokenizer = AutoTokenizer.from_pretrained(pretrained_checkpoint, use_fast=True)
-model = BertForSequenceClassification.from_pretrained(pretrained_checkpoint, id2label=id2label)
+if args.roberta:
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_checkpoint, use_fast=True)
+    model = RobertaForSequenceClassification.from_pretrained(pretrained_checkpoint, id2label=id2label)
+else: 
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_checkpoint, use_fast=True)
+    model = BertForSequenceClassification.from_pretrained(pretrained_checkpoint, id2label=id2label)
 
 # LOAD DATA
 print("Loading data...")
@@ -50,7 +55,7 @@ training_args = TrainingArguments(
     save_strategy = "epoch",
     # push_to_hub=True,             # can push to hub instead of saving locally
     learning_rate=2e-5,             # defaults to Adam optimizer
-    num_train_epochs=5,             # default is 3. BERT authors recommend 2-4
+    num_train_epochs=4,             # default is 3. BERT authors recommend 2-4
     logging_steps=1,                # to log loss from the first epoch
     load_best_model_at_end=True,
     metric_for_best_model="eval_f1",# default is loss
