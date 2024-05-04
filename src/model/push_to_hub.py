cd397ff85c682d5a5a96e7699dfa06b80dd5731a
syntaxshill <syntaxshill@gmail.com>
2024-05-04 14:36:30 -0400

    added roberta support to push_to_hub

--- src/model/push_to_hub.py
+++ src/model/push_to_hub.py
@@ -1,5 +1,5 @@
 import argparse
-from transformers import BertForSequenceClassification
+from transformers import BertForSequenceClassification, RobertaForSequenceClassification
 from pathlib import Path
 
 def dir_path(path):
@@ -12,11 +12,17 @@ def dir_path(path):
 parser = argparse.ArgumentParser(description='Push finetuned model to hub.')
 parser.add_argument("-p", '--model-path', type=dir_path, help='Model Path to push to hub')
 parser.add_argument("-n", '--model-name', type=str, help='Name for the model being pushed')
+parser.add_argument('--roberta', action='store_true', help='Whether to use RoBERTa or BERT(default).')
 args = parser.parse_args()
 
-model = BertForSequenceClassification.from_pretrained(
-    pretrained_model_name_or_path=args.model_path
-)
+if args.roberta:
+    model = RobertaForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path=args.model_path,
+    )
+else:
+    model = BertForSequenceClassification.from_pretrained(
+        pretrained_model_name_or_path=args.model_path
+    )
 
 model.push_to_hub(
     args.model_name
