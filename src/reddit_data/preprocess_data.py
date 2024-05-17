62561cc999dd8a5a7770ad8d71545125fd9990db
Cat Ball <43632885+catleeball@users.noreply.github.com>
2024-05-17 16:52:12 -0700

    Update pipeline, bugfixes, two write steps

--- src/reddit_data/preprocess_data.py
+++ src/reddit_data/preprocess_data.py
@@ -1,5 +1,6 @@
 import re
 import string
+from typing import Tuple
 from datatrove.data import DocumentsPipeline, Document
 from datatrove.executor import LocalPipelineExecutor
 from datatrove.pipeline.base import PipelineStep
@@ -9,18 +10,25 @@ from datatrove.pipeline.writers import JsonlWriter
 from urlextract import URLExtract
 
 
-INPUT_DIR = '/Volumes/media/Data/reddit/filtered_by_subreddit'
-CLEANED_DIR = '/Volumes/media/Data/reddit/cleaned_data'
-TONE_INDICATOR_DIR = '/Volumes/media/Data/reddit/tone_indicator_posts'
-LOG_DIR = '/Volumes/media/Data/reddit/logs'
+INPUT_DIR = '/Media/Data/reddit/Selected_Subreddits_Data_Small_Chunked_Files_Compressed'
+CLEANED_DIR = '/Media/Data/reddit/Cleaned_Data'
+TONE_INDICATOR_DIR = '/Media/Data/reddit/Tone_Indicator_Data'
+LOG_DIR = '/Media/Data/reddit/.logs'
 # URL_REGEX = re.compile(r"^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
 # URL_REGEX = re.compile(r'(http\S+)')
-TONE_INDICATOR_REGEX = re.compile(r'(/s[\s$]|/sarcasm|/sarcastic|/serious|/srs)')
+# TONE_INDICATOR_REGEX = re.compile(r'([^\S]/s[\S$]|[^\S]/sarcasm[\S$]|[^\S]/sarcastic[\S$]|[^\S]/serious[\S$]|[^\S]/srs[\S$])')
+SARCASM_INDICATOR_REGEX = re.compile(r'([^\S][/\\]s[\S$.,?!]|[^\S][/\\]sarcasm[\S$.,?!]|[^\S][/\\]sarcastic[\S$.,?!]|)')
+SERIOUS_INDICATOR_REGEX = re.compile(r'([^\S][/\\]serious[\S$.,?!]|[^\S][/\\]srs[\S$.,?!])')
+
+BAD_ID = 'BAD'
+BAD_JSON = {'text': '', 'id': BAD_ID, 'metadata': {}}
+
 
 class RemoveNonASCII(PipelineStep):
-    name = "Remove non-ASCII"
+    name = 'Remove non-ASCII'
+    type = 'Transformer'
 
-    def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
+    def run(self, data: DocumentsPipeline, rank: int = 0, word_size: int = 1) -> DocumentsPipeline:
         for doc in data:
             with self.track_time():
                 doc.text = str(filter(lambda c: c in string.printable, doc.text))
@@ -28,9 +36,10 @@ class RemoveNonASCII(PipelineStep):
 
 
 class RemoveURLs(PipelineStep):
-    name = "Remove URLs"
+    name = 'Remove URLs'
+    type = 'Transformer'
 
-    def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
+    def run(self, data: DocumentsPipeline, rank: int = 0, word_size: int = 1) -> DocumentsPipeline:
         for doc in data:
             with self.track_time():
                 url_extractor = URLExtract()
@@ -49,53 +58,77 @@ class RemoveURLs(PipelineStep):
 
 
 class ToneIndicatorFilter(BaseFilter):
-    name = "Tone Indicator"
+    name = 'Tone Indicator'
 
     def filter(self, doc: Document) -> bool:
-        return bool(re.search(TONE_INDICATOR_REGEX, doc.text))
+        with self.track_time():
+            sarcastic = bool(re.search(SARCASM_INDICATOR_REGEX, doc.text))
+            serious = bool(re.search(SERIOUS_INDICATOR_REGEX, doc.text))
+            self.stat_update("sarcastic", value=sarcastic)
+            self.stat_update("serious", value=serious)
+        yield sarcastic or serious
 
 
-def clean():
-    """Don't try to find tone indicators, just clean the data"""
-    pipeline = [
-        # Parse 'id' and 'selftext' attributes from each line Document objects
-        JsonlReader(
-            data_folder=INPUT_DIR,
-            id_key='id',
-            text_key='selftext',
-        ),
-        # Clean data in documents in-place
-        RemoveNonASCII(),
-        RemoveURLs(),
-        JsonlWriter(
-            output_folder=CLEANED_DIR,
-            output_filename='data.gz',
-            compression='gzip'
-        )
-    ]
-    executor = LocalPipelineExecutor(
-        pipeline=pipeline,
-        logging_dir=LOG_DIR,
-        workers=24,
-    )
-    executor.run()
+class EmptyPostFilter(BaseFilter):
+    name = 'Filter Empty Posts'
+
+    def filter(self, doc: Document) -> bool | Tuple[bool, str]:
+        if doc.id == BAD_ID:
+            yield False
+        # We only want text long enough to fit a tone indicator
+        match doc.text.strip():
+            case None | '' | '[deleted]' | '[removed]':
+                yield False
+            case _:
+                yield True
+
+
+def jsonl_text_extraction_adapter(data: dict, path: str, id_in_file: int | str) -> dict:
+    # Comment objects have the `body` attribute
+    if 'body' in data:
+        text_key = data.pop('body')
+    elif 'selftext' in data:
+        text_key = data.pop('selftext')
+    else:
+        return BAD_JSON
+    return {
+        'text': text_key,
+        'id': data.pop('id', f'{path}/{id_in_file}'),
+        # "metadata": data.pop("metadata", {}) | data,  # remaining data goes into metadata
+        'metadata': {
+            'author': data.get('author', ''),
+            'title': data.get('title', ''),
+            'subreddit': data.get('subreddit', ''),
+            'subreddit_id': data.get('subreddit_id', ''),
+            'permalink': data.get('permalink', ''),
+            'created_utc': data.get('created_utc', ''),
+        }
+    }
 
 
 def clean_and_classify():
     pipeline = [
-        # Parse 'id' and 'selftext' attributes from each line Document objects
+        # Parse 'id' and 'selftext'/'body' attributes from each line Document objects
         JsonlReader(
             data_folder=INPUT_DIR,
-            id_key='id',
-            text_key='selftext',
+            adapter=jsonl_text_extraction_adapter,
         ),
+        # Remove empty / invalid posts
+        EmptyPostFilter(),
         # Clean data in documents in-place
         RemoveNonASCII(),
         RemoveURLs(),
-        # Keep only documents containing tone indicators
-        ToneIndicatorFilter(),
+        # Write all cleaned data before proceeding to tone classification.
         JsonlWriter(
             output_folder=CLEANED_DIR,
+            output_filename='data.gz',
+            compression='gzip'
+        ),
+        # Keep only documents containing tone indicators
+        ToneIndicatorFilter(),
+        # Write data containing tone indicators
+        JsonlWriter(
+            output_folder=TONE_INDICATOR_DIR,
             output_filename='data.gz',
             compression='gzip'
         )
@@ -108,5 +141,4 @@ def clean_and_classify():
 
 
 if __name__ == '__main__':
-    # clean()
     clean_and_classify()
